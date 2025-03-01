6cd8adcfb4e05e50df5c7a6c5ca7cb2404027023
Zirconium419122 <rasmus@liaskar.net>
2025-03-01 12:42:15 +0100

    Initial commit

---
+++ src/engine.rs
@@ -0,0 +1,125 @@
+use std::{io, str::FromStr};
+
+use chessframe::{bitboard::EMPTY, board::Board, uci::*};
+
+pub struct Engine {
+    board: Board,
+    quitting: bool,
+}
+
+impl Uci for Engine {
+    fn send_command(&mut self, command: UciCommand) {
+        match command {
+            UciCommand::Id { name, author } => {
+                println!("id name {}", name);
+                println!("id author {}", author);
+            }
+            UciCommand::UciOk => {
+                println!("uciok");
+            }
+            UciCommand::ReadyOk => {
+                println!("readyok");
+            }
+            UciCommand::BestMove { best_move, ponder } => {
+                if let Some(ponder) = ponder {
+                    println!("bestmove {} ponder {}", best_move, ponder);
+                } else {
+                    println!("bestmove {}", best_move);
+                }
+            }
+            UciCommand::Info(info) => {
+                println!("{}", info);
+            }
+            _ => {}
+        }
+    }
+
+    fn read_command(&mut self) -> Option<UciCommand> {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        UciCommand::from_str(line.trim()).ok()
+    }
+
+    fn handle_command(&mut self) {
+        if let Some(command) = self.read_command() {
+            match command {
+                UciCommand::Uci => {
+                    self.send_command(UciCommand::Id {
+                        name: "Ferrischess".to_string(),
+                        author: "Zirconium419122".to_string(),
+                    });
+                    self.send_command(UciCommand::UciOk);
+                }
+                UciCommand::Debug(debug) => {
+                    if debug {
+                        self.send_command(UciCommand::Info(Info {
+                            string: Some("Debug mode not supported yet!".to_string()),
+                            ..Default::default()
+                        }));
+                    }
+                }
+                UciCommand::IsReady => self.send_command(UciCommand::ReadyOk),
+                UciCommand::UciNewGame => self.board = Board::default(),
+                UciCommand::Position { fen, moves } => {
+                    if fen == "startpos" {
+                        self.board = Board::default();
+                    } else {
+                        self.board = Board::from_fen(&fen);
+                    };
+
+                    if let Some(moves) = moves {
+                        let board = &mut self.board;
+
+                        for mv in moves {
+                            let mv = board.infer_move(&mv).unwrap();
+
+                            let _ = board.make_move(&mv);
+                        }
+                    }
+                }
+                UciCommand::Go { .. } => {
+                    let moves = self
+                        .board
+                        .generate_moves_vec(!EMPTY)
+                        .into_iter()
+                        .filter(|mv| self.board.make_move_new(mv).is_ok())
+                        .collect::<Vec<_>>();
+
+                    if let Some(mv) = moves.last() {
+                        self.send_command(UciCommand::Info(Info {
+                            pv: Some(mv.to_string()),
+                            ..Default::default()
+                        }));
+                        self.send_command(UciCommand::BestMove {
+                            best_move: mv.to_string(),
+                            ponder: None,
+                        });
+                    }
+                }
+                UciCommand::Stop => {}
+                UciCommand::Quit => self.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Engine {
+    pub fn new() -> Engine {
+        Engine {
+            board: Board::default(),
+            quitting: false,
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            self.handle_command();
+
+            if self.quitting {
+                break;
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use engine::Engine;
+
+mod engine;
+
+fn main() {
+    let mut engine = Engine::new();
+    engine.run();
+}
