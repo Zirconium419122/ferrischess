a65be6de3fe34706e2370d7cbd82874ec83c9b6c
Zirconium419122 <rasmus@liaskar.net>
2025-03-09 17:30:40 +0100

    Update version number in Cargo.toml; convert Eval from tuple struct to field struct

--- src/eval.rs
+++ src/eval.rs
@@ -6,13 +6,15 @@ use chessframe::{
 
 pub const PIECE_VALUES: [i32; 6] = [100, 310, 325, 500, 900, 0];
 
-pub struct Eval<'a>(&'a Board);
+pub struct Eval<'a> {
+    board: &'a Board,
+}
 
 impl<'a> Eval<'a> {
     pub const MATE_SCORE: i32 = 1_000_000_000;
 
     pub fn new(board: &Board) -> Eval {
-        Eval(board)
+        Eval { board }
     }
 
     pub fn piece_value(piece: &Piece) -> i32 {
@@ -23,17 +25,17 @@ impl<'a> Eval<'a> {
         let mut score = 0;
 
         for piece in PIECES.iter() {
-            score += self.0.pieces_color(*piece, Color::White).count_ones() as i32
+            score += self.board.pieces_color(*piece, Color::White).count_ones() as i32
                 * Self::piece_value(piece);
-            score -= self.0.pieces_color(*piece, Color::Black).count_ones() as i32
+            score -= self.board.pieces_color(*piece, Color::Black).count_ones() as i32
                 * Self::piece_value(piece);
         }
 
-        if self.0.in_check() {
+        if self.board.in_check() {
             score -= 50;
         }
 
-        let perspective = if self.0.side_to_move == Color::White {
+        let perspective = if self.board.side_to_move == Color::White {
             1
         } else {
             -1
