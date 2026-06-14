f5c1580c88465cb3b3c762d0e81a803bbb7bb38c
Zirconium419122 <rasmus@liaskar.net>
2026-06-14 19:50:00 +0200

    Implement Mobility scoring in Eval::mobility_score

--- src/eval.rs
+++ src/eval.rs
@@ -1,10 +1,10 @@
 use chessframe::{
-    bitboard::EMPTY,
+    bitboard::{BitBoard, EMPTY},
     board::Board,
     color::Color,
     file::File,
-    magic::{FILES, get_adjacent_files},
-    piece::Piece,
+    magic::{FILES, get_adjacent_files, get_bishop_moves, get_knight_moves, get_rook_moves},
+    piece::{PIECES, Piece}, square::Square,
 };
 
 use crate::piecesquaretable::PieceSquareTable;
@@ -28,19 +28,24 @@ impl Eval<'_> {
 
     pub fn eval(&self) -> i32 {
         let mut score = 0;
+        let mut mobility_score = 0;
 
         let game_phase = Self::calculate_game_phase(self.board);
 
-        for square in self.board.occupancy(Color::White) {
-            let piece = unsafe { self.board.get_piece(square).unwrap_unchecked() };
-            score += Self::piece_value(piece)
-                + PieceSquareTable::read(square, piece, Color::White, game_phase) as i32;
-        }
+        for piece in PIECES {
+            for square in self.board.pieces_color(piece, Color::White) {
+                score += Self::piece_value(piece)
+                    + PieceSquareTable::read(square, piece, Color::White, game_phase) as i32;
 
-        for square in self.board.occupancy(Color::Black) {
-            let piece = unsafe { self.board.get_piece(square).unwrap_unchecked() };
-            score -= Self::piece_value(piece)
-                + PieceSquareTable::read(square, piece, Color::Black, game_phase) as i32;
+                mobility_score += self.mobility_score(square, piece, Color::White);
+            }
+
+            for square in self.board.pieces_color(piece, Color::Black) {
+                score -= Self::piece_value(piece)
+                    + PieceSquareTable::read(square, piece, Color::Black, game_phase) as i32;
+
+                mobility_score -= self.mobility_score(square, piece, Color::Black);
+            }
         }
 
         score += self.pawn_structure_score(Color::White);
@@ -52,6 +57,8 @@ impl Eval<'_> {
         if self.board.in_check() {
             score -= 50;
         }
+
+        score += mobility_score;
 
         if self.board.side_to_move == Color::White {
             score
@@ -87,7 +94,7 @@ impl Eval<'_> {
         let mut score = 0;
 
         if self.board.pieces_color(Piece::Bishop, color).count_ones() >= 2 {
-            score += (10 * game_phase + 35 * (24 - game_phase)) / 24;
+            score += (30 * game_phase + 80 * (24 - game_phase)) / 24;
         }
 
         if self.board.pieces_color(Piece::Knight, color).count_ones() >= 2 {
@@ -98,6 +105,29 @@ impl Eval<'_> {
             score
         } else {
             -score
+        }
+    }
+
+    pub fn mobility_score(&self, square: Square, piece: Piece, color: Color) -> i32 {
+        let allied_pieces = self.board.occupancy(color);
+        let combined = self.board.combined();
+
+        let pawn_attacks = self.pawn_attacks(!color);
+
+        let mobility = (match piece {
+            Piece::Knight => get_knight_moves(square),
+            Piece::Bishop => get_bishop_moves(square, combined),
+            Piece::Rook => get_rook_moves(square, combined),
+            Piece::Queen => get_bishop_moves(square, combined) | get_rook_moves(square, combined),
+            _ => return 0,
+        } & !allied_pieces & !pawn_attacks).count_ones() as i32;
+
+        match piece {
+            Piece::Knight => mobility * 4,
+            Piece::Bishop => mobility * 5,
+            Piece::Rook => mobility * 2,
+            Piece::Queen => mobility,
+            _ => unreachable!(),
         }
     }
 
@@ -117,5 +147,20 @@ impl Eval<'_> {
 
     pub fn mate_score(score: i32) -> bool {
         score.abs() >= Eval::MATE_SCORE - 1000
+    }
+
+    fn pawn_attacks(&self, color: Color) -> BitBoard {
+        match color {
+            Color::White => {
+                ((self.board.pieces_color(Piece::Pawn, color) << 7) & !BitBoard(0x8080808080808080))
+                    | ((self.board.pieces_color(Piece::Pawn, color) << 9)
+                        & !BitBoard(0x1010101010101010))
+            }
+            Color::Black => {
+                ((self.board.pieces_color(Piece::Pawn, color) >> 7) & !BitBoard(0x1010101010101010))
+                    | ((self.board.pieces_color(Piece::Pawn, color) >> 9)
+                        & !BitBoard(0x8080808080808080))
+            }
+        }
     }
 }
